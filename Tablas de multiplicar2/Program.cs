cbfcc50fa88ea14aa343e62272a04518f8795d8b
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-21 21:55:30 -0500

    Actualizar archivos binarios y cachés en múltiples directorios

--- Tablas de multiplicar2/Program.cs
+++ Tablas de multiplicar2/Program.cs
@@ -1,15 +1,28 @@
-﻿Console.Write("Ingresa un Numero: ");
+﻿bool On = true;
+Console.Write("Ingresa un Numero: ");
 string numero = Console.ReadLine();
 int num = int.Parse(numero);
 
-if(num >= 1 && num <= 12)
+while (On)
 {
-
-  for (int i = 1; i <= 12; i++)
+  Console.WriteLine("");
+  if (num >= 1)
   {
-    Console.WriteLine($"{num} x {i} = {num * i}");
+    Console.WriteLine($"La Tabla De Multiplicar Del {num} es: ");
+    for (int i = 1; i <= 12; i++)
+    {
+      Console.WriteLine($"{num} x {i} = {num * i}");
+    }
+    On = false;
   }
+  else
+  {
+    Console.WriteLine("El numero debe ser mayor a 0");
+    Console.Write("Ingresa un Numero: ");
+    numero = Console.ReadLine();
+    num = int.Parse(numero);
 
+  }
 }
 
 Console.Read();
