65d029ad34589ec3066bf66bf8a477803729dc1d
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-28 19:08:23 -0500

    Agregar archivos de proyecto y actualizar cachés para Ejercicio9, Ejercicio10 y EjercicioProductos

--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd59e75b58d5180aabf35c47bd0ba265688bab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead976073cc8da1faecf5eb4f89a1d63c3ba383c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
